8141f5965aabe61d61e3bfcc31ab2ff89ec00288
areebatahir358 <areebatahir966@gmail.com>
2025-06-12 14:03:07 +0500

    Initial commit or update

--- app/services/image_editor_service.rb
+++ app/services/image_editor_service.rb
@@ -1,15 +1,16 @@
-
 class ImageEditorService
   def self.add_text(image_path:, output_path:, text:)
     image = MiniMagick::Image.open(image_path)
 
     image.combine_options do |c|
-      c.gravity "NorthWest" # top-left corner
-      c.pointsize "24"
+      c.font "Arial-Bold"            # Use a font installed on your system
+      c.gravity "Center"             # Center of the image (horizontal + vertical)
+      c.pointsize "36"               # Bigger font size
       c.fill "black"
-      c.draw "text 50,100 '#{text[:name]}'"
-      c.draw "text 50,150 '#{text[:date]}'"
-      c.draw "text 50,200 '#{text[:message]}'"
+
+      # Each line offset using \n and Y axis shift with annotate (alternative to multiple draw calls)
+      multiline_text = "#{text[:name]}\n#{text[:date]}\n#{text[:message]}"
+      c.draw "text 0,0 '#{multiline_text}'"
     end
 
     image.write output_path
